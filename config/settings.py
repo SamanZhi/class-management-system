951c310e44e915647589767f5bfe358e0c5a5855
Saman Zhiani <sampayne69@gmail.com>
2026-08-05 12:44:07 +0330

    create User model

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+
+    # Local apps
+    'users',
+]   
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -115,3 +118,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Custom user model
+AUTH_USER_MODEL = 'users.USER'
---
+++ users/models.py
@@ -0,0 +1,36 @@
+from django.contrib.auth.models import AbstractUser
+from django.core.exceptions import ValidationError
+from django.core.validators import RegexValidator
+from django.db import models
+
+
+class User(AbstractUser):
+    class Role(models.TextChoices):
+        TEACHER = 'teacher', 'Teacher'
+        EDUCATION_OFFICER = 'education_officer', 'Education_Officer'
+        FINANCE_OFFICER = 'finance_officer', 'Finance_Officer'
+
+    phone_regex = RegexValidator(
+        regex=r'^\+98\d{10}$',
+        message="Phone number must be in format '+98XXXXXXXXXX' (10 digits after +98)."
+    )
+
+    role = models.CharField(max_length=20, choices=Role.choices)
+    phone_number = models.CharField(validators=[phone_regex], max_length=15, blank=True, null=True)
+    emergency_number = models.CharField(validators=[phone_regex], max_length=15, blank=True, null=True)
+
+    def clean(self):
+        super().clean()
+        if self.role == 'teacher':
+            errors = {}
+            if not self.phone_number:
+                errors['phone_number'] = 'Phone number is required for teachers.'
+            if not self.emergency_number:
+                errors['emergency_number'] = 'Emergency number is required for teachers.'
+            if errors:
+                raise ValidationError(errors)
+
+    def __str__(self):
+        return f"{self.username} ({self.get_role_display()})"
+
+
