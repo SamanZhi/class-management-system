2c852bc70488948fe923862f75dd18ba8a8fe45f
Saman Zhiani <sampayne69@gmail.com>
2026-08-06 23:02:43 +0330

    add login success test

---
+++ users/tests/test_authentication.py
@@ -0,0 +1,25 @@
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from users.models import User
+
+
+class AuthenticationTest(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='saman_teacher',
+            password='pass123',
+            role='teacher'
+        )
+        self.login_url = '/api/users/login/'
+
+    def test_login_success(self):
+        response = self.client.post(self.login_url, {
+            'username': 'saman_teacher',
+            'password': 'pass123'
+        })
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn('access', response.data)
+        self.assertIn('refresh', response.data)
+
