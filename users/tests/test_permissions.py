d13854084dc9d005cdf0bd7e069dfbab93df8629
Saman Zhiani <sampayne69@gmail.com>
2026-08-07 00:37:31 +0330

    add role access own section test

---
+++ users/tests/test_permissions.py
@@ -0,0 +1,24 @@
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from users.models import User
+
+
+class PermissionTest(APITestCase):
+    def setUp(self):
+        self.teacher = User.objects.create_user(
+            username='saman_teacher',
+            password='pass123',
+            role='teacher'
+        )
+        self.education_officer = User.objects.create_user(
+            username='saman_edu',
+            password='pass456',
+            role='education_officer'
+        )
+
+    def test_teacher_can_access_own_section(self):
+        self.client.force_authenticate(user=self.teacher)
+        response = self.client.get('/api/teachers/...')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
