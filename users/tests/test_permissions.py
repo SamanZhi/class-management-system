a0fb3dd85e2924eedd85b0a859ea3f7885e95dd3
Saman Zhiani <sampayne69@gmail.com>
2026-08-11 23:20:50 +0330

    add education officer role dashboard access test

--- users/tests/test_permissions.py
+++ users/tests/test_permissions.py
@@ -44,3 +44,24 @@ class PermissionTest(APITestCase):
         response = self.client.get(self.finance_url)
 
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    
+    def test_education_officer_can_access_education_dashboard(self):
+        self.client.force_authenticate(user=self.education_officer)
+        response = self.client.get(self.education_url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_education_officer_cannot_access_teacher_dashboard(self):
+        self.client.force_authenticate(user=self.education_officer)
+        response = self.client.get(self.teacher_url)
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_education_officer_cannot_access_finance_dashboard(self):
+        self.client.force_authenticate(user=self.education_officer)
+        response = self.client.get(self.finance_url)
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    
