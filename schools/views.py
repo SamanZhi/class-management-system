29d70d7b2b0ce85bbbcec3a1953ad10e9f33de97
Saman Zhiani <sampayne69@gmail.com>
2026-08-12 23:05:32 +0330

    add school create view

--- schools/views.py
+++ schools/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from users.permissions import IsEducationOfficerOrReadOnly
+
+from .models import School
+from .serializers import SchoolSerializer
+
+
+class SchoolListCreateView(APIView):
+    permission_classes = [IsEducationOfficerOrReadOnly]
+
+    def get(self, request):
+        schools = School.objects.all()
+        serializer = SchoolSerializer(schools, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = SchoolSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
