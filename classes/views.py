3aa34d70c1d4663bbcddc01c768801db4f9ca289
Saman Zhiani <sampayne69@gmail.com>
2026-08-13 17:35:57 +0330

    add class create view

--- classes/views.py
+++ classes/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from users.permissions import IsEducationOfficerOrReadOnly
+
+from .models import Class, ClassTeacher
+from .serializers import (
+    ClassDetailSerializer,
+    ClassSerializer,
+    ClassTeacherSerializer,
+)
+
+
+class ClassListCreateView(APIView):
+    permission_classes = [IsEducationOfficerOrReadOnly]
+
+    def get(self, request):
+        classes = Class.objects.all()
+        serializer = ClassSerializer(classes, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = ClassSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
