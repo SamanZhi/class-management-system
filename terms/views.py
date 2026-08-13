a6890323644aae2307db094aaf367634d9a2e4c3
Saman Zhiani <sampayne69@gmail.com>
2026-08-13 16:53:27 +0330

    add term create view

--- terms/views.py
+++ terms/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from users.permissions import IsEducationOfficerOrReadOnly
+
+from .models import Term
+from .serializers import TermSerializer
+
+
+class TermListCreateView(APIView):
+    permission_classes = [IsEducationOfficerOrReadOnly]
+
+    def get(self, request):
+        terms = Term.objects.all()
+        serializer = TermSerializer(terms, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = TermSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
