bfc582198185663003c8dbe2c965e2ed3e9545ad
Saman Zhiani <sampayne69@gmail.com>
2026-08-14 13:28:51 +0330

    add school post view test

--- schools/tests/test_views.py
+++ schools/tests/test_views.py
@@ -30,3 +30,18 @@ class SchoolViewTestCase(APITestCase):
         self.client.force_authenticate(self.teacher)
         response = self.client.get(self.list_url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_create_denied_for_teacher(self):
+        self.client.force_authenticate(self.teacher)
+        response = self.client.post(self.list_url, {'name': 'New School'})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_create_allowed_for_education_officer(self):
+        self.client.force_authenticate(self.education_officer)
+        response = self.client.post(self.list_url, {'name': 'New School'})
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_create_denied_for_finance_officer(self):
+        self.client.force_authenticate(self.finance_officer)
+        response = self.client.post(self.list_url, {'name': 'New School'})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
