3186a9e8744b4d0a7cfd38e7b7a7f6afe6b95b77
Saman Zhiani <sampayne69@gmail.com>
2026-08-14 14:42:25 +0330

    add school management to admin panel

--- schools/admin.py
+++ schools/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import School
+
+
+@admin.register(School)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address', 'is_active')
+    
+    list_filter = ('is_deleted',)
+    
+    search_fields = ('name',)
+    
+    ordering = ('name',)
