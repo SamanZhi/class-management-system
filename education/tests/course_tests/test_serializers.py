a8d06b605a93ddc62cd0ff7631933068c3723be3
Saman Zhiani <sampayne69@gmail.com>
2026-08-19 14:33:19 +0330

    set up course detail serializers test

--- education/tests/course_tests/test_serializers.py
+++ education/tests/course_tests/test_serializers.py
@@ -271,3 +271,31 @@ class CourseTeacherSerializerTests(TestCase):
             serializer.is_valid(), 
             serializer.errors
         )
+
+
+class CourseDetailsSerializerTests(TestCase):
+    def setUp(self):
+            self.school = School.objects.create(name='School A', address='Address A')
+    
+            self.term = Term.objects.create(
+                start_date=date(2026, 9, 1),
+                end_date=date(2026, 11, 31),
+                type='regular'
+            )
+    
+            self.course = Course.objects.create(
+                school=self.school,
+                term=self.term,
+                subject='Python',
+                duration=90
+            )
+            
+            self.teacher = User.objects.create_user(
+                username='saman_teacher',
+                password='pass123',
+                role='teacher',
+                first_name='Saman',
+                last_name='Zhiani',
+                phone_number='=+989361208772',
+                emergency_number='+989876543210'
+            )
