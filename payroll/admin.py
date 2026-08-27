4314b6e7a4f65f456e488876c32c626b6f97faac
Saman Zhiani <sampayne69@gmail.com>
2026-08-27 06:38:47 +0330

    add class TeacherTermRate and PayrollRecord to admin panel management

--- payroll/admin.py
+++ payroll/admin.py
@@ -1,3 +1,73 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import PayrollRecord, TeacherTermRate
+
+
+@admin.register(TeacherTermRate)
+class TeacherTermRateAdmin(admin.ModelAdmin):
+    list_display = (
+        'teacher',
+        'term',
+        'base_rate',
+        'created_at',
+    )
+
+    list_filter = (
+        'term',
+    )
+
+    search_fields = (
+        'teacher__username',
+    )
+
+    ordering = (
+        '-term__start_date',
+        'teacher__username',
+    )
+
+    list_select_related = (
+        'teacher',
+        'term',
+    )
+
+
+@admin.register(PayrollRecord)
+class PayrollRecordAdmin(admin.ModelAdmin):
+    list_display = (
+        'teacher',
+        'year',
+        'month',
+        'amount',
+        'sessions_60',
+        'sessions_90',
+        'sessions_120',
+    )
+
+    list_filter = (
+        'year',
+        'month',
+    )
+
+    search_fields = (
+        'teacher__username',
+    )
+
+    ordering = (
+        '-year',
+        '-month',
+        'teacher__username',
+    )
+
+    list_select_related = (
+        'teacher',
+    )
+
+    readonly_fields = (
+        'teacher',
+        'year',
+        'month',
+        'amount',
+        'sessions_60',
+        'sessions_90',
+        'sessions_120',
+    )
